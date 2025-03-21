30b690547d07265172449ddb2739e1fc6c2d03fa
Victory-S29 <viktoriia.sanina.2917@gmail.com>
2025-03-21 21:59:19 +0100

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import { MainPage } from "./dev/";
 
 function App() {
   return (
     <div className="App">
-     
+      <MainPage />
     </div>
   );
 }
---
+++ src/dev/pages/MainPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { NavBar } from '../';
+
+const MainPage = () => {
+    return (
+        <NavBar />
+    );
+};
+
+export default MainPage;
