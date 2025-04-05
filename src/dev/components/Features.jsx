a2ae23be429f564f3c88d83e69446254d59c7614
Victory-S29 <viktoriia.sanina.2917@gmail.com>
2025-04-05 19:42:58 +0200

    fixed links to images

--- src/dev/components/Features.jsx
+++ src/dev/components/Features.jsx
@@ -1,11 +1,11 @@
 import React, { Fragment } from 'react';
 
-const explore = "https://raw.githubusercontent.com/Victory-S29/libra-personal-library/main/src/style/images/communityImgs/explore.jpg";
-const progress = "https://raw.githubusercontent.com/Victory-S29/libra-personal-library/main/src/style/images/communityImgs/progress.jpg";
-const save = "https://raw.githubusercontent.com/Victory-S29/libra-personal-library/main/src/style/images/communityImgs/save.jpg";
-const inspiration = "https://raw.githubusercontent.com/Victory-S29/libra-personal-library/main/src/style/images/communityImgs/inspiration.jpg";
-const recommendation = "https://raw.githubusercontent.com/Victory-S29/libra-personal-library/main/src/style/images/communityImgs/recommendation.jpg";
-const start = "https://raw.githubusercontent.com/Victory-S29/libra-personal-library/main/src/style/images/communityImgs/start.jpg";
+const explore = "https://raw.githubusercontent.com/Victory-S29/libra-personal-library/main/src/style/images/featuresImgs/explore.png";
+const progress = "https://raw.githubusercontent.com/Victory-S29/libra-personal-library/main/src/style/images/featuresImgs/progress.png";
+const save = "https://raw.githubusercontent.com/Victory-S29/libra-personal-library/main/src/style/images/featuresImgs/save.png";
+const inspiration = "https://raw.githubusercontent.com/Victory-S29/libra-personal-library/main/src/style/images/featuresImgs/inspiration.png";
+const recommendation = "https://raw.githubusercontent.com/Victory-S29/libra-personal-library/main/src/style/images/featuresImgs/recommendation.png";
+const start = "https://raw.githubusercontent.com/Victory-S29/libra-personal-library/main/src/style/images/featuresImgs/start.png";
 
 const Features = () => {
     const communityAdvantages = [
