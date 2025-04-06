171dd55d3cd3b09bf374f1c0ddb494b87b7d0cc9
Victory-S29 <viktoriia.sanina.2917@gmail.com>
2025-04-06 21:37:07 +0200

    added login popup with design

--- src/dev/components/NavBar.jsx
+++ src/dev/components/NavBar.jsx
@@ -3,7 +3,7 @@ import LibraLogo from '../../style/images/LibraLogo.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser } from '@fortawesome/free-regular-svg-icons';
 
-const NavBar = () => {
+const NavBar = ({ SetShowLogin, toggleLoginPopup }) => {
     return (
         <Fragment>
             <nav className='main-nav'>
@@ -12,7 +12,7 @@ const NavBar = () => {
                     <h1>Libra</h1>
                 </div>
                 <div className='navbar-settings'>
-                    <FontAwesomeIcon icon={faUser} className='icon' />
+                    <FontAwesomeIcon icon={faUser} className='icon' onClick={() => { SetShowLogin(true); toggleLoginPopup(); }} />
                 </div>
             </nav>
             <hr className='separator' />
--- src/dev/pages/MainPage.jsx
+++ src/dev/pages/MainPage.jsx
@@ -1,17 +1,28 @@
-import React, { Fragment } from 'react';
-import { Catalogue, Features, NavBar, SliderComponent } from '../';
+import React, { Fragment, useState } from 'react';
+import { Catalogue, Features, LoginPopup, NavBar, SliderComponent } from '../';
 import { getPopularBooksSelector, getStartingBooksSelector } from '../../store/reducers/books.reducer';
 import { useSelector } from 'react-redux';
 
 const MainPage = () => {
     const startingBooks = useSelector(getStartingBooksSelector);
     const popularBooks = useSelector(getPopularBooksSelector);
-     
+
+    const [currentLoginState, SetCurrentLoginState] = useState("Sign up");
+    const [showLogin, SetShowLogin] = useState(false);
+
+    const toggleLoginPopup = () => {
+        const scrollBarWidth = window.innerWidth - document.documentElement.clientWidth;
+        document.body.style.paddingRight = showLogin ? '' : `${scrollBarWidth}px`;
+        document.body.style.overflow = showLogin ? '' : 'hidden';
+    };
+
     return (
         <Fragment>
-            <NavBar />
-            <SliderComponent {...startingBooks}   />
-            <SliderComponent {...popularBooks}   />
+            {showLogin ? <LoginPopup SetShowLogin={SetShowLogin} toggleLoginPopup={toggleLoginPopup}
+                currentLoginState={currentLoginState} SetCurrentLoginState={SetCurrentLoginState} /> : <></>}
+            <NavBar SetShowLogin={SetShowLogin} toggleLoginPopup={toggleLoginPopup} />
+            <SliderComponent {...startingBooks} />
+            <SliderComponent {...popularBooks} />
             <Catalogue />
             <Features />
         </Fragment>
