6471e72847219adbd00496095a505ab0533838a6
Victory-S29 <viktoriia.sanina.2917@gmail.com>
2025-04-21 14:58:41 +0200

    added main layout for tech page

--- src/dev/pages/TechnologiesPage.jsx
+++ src/dev/pages/TechnologiesPage.jsx
@@ -1,10 +1,69 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { getTechnologiesEnSelector } from '../../store/reducers/languages.reducer';
+import { useSelector } from 'react-redux';
+import { useLocation } from 'react-router-dom';
 
 const TechnologiesPage = () => {
+    const tech = useSelector(getTechnologiesEnSelector);
+
+    const { pathname } = useLocation();
+
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [pathname]);
+
     return (
-        <div>
-            123
-        </div>
+        <section className='technologies-page'>
+            <section className='technologies-intro'>
+                <h1>{tech.intro.title}</h1>
+                <p>{tech.intro.description}</p>
+            </section>
+            <section className='technologies-framework-section'>
+                <h4>{tech.framework.title}</h4>
+                <ul>
+                    {tech.framework.points.map((item) => <li><p>{item}</p></li>)}
+                </ul>
+            </section>
+            <section className='technologies-routing-section'>
+                <h4>{tech.routing.title}</h4>
+                <ul>
+                    {tech.routing.points.map((item) => <li><p>{item}</p></li>)}
+                </ul>
+            </section>
+            <section className='technologies-stateManagement-section'>
+                <h4>{tech.stateManagement.title}</h4>
+                <ul>
+                    {tech.stateManagement.points.map((item) => <li><p>{item}</p></li>)}
+                </ul>
+            </section>
+            <section className='technologies-persistence-section'>
+                <h4>{tech.persistence.title}</h4>
+                <ul>
+                    {tech.persistence.points.map((item) => <li><p>{item}</p></li>)}
+                </ul>
+            </section>
+            <section className='technologies-dataManagement-section'>
+                <h4>{tech.dataManagement.title}</h4>
+                <ul>
+                    {tech.dataManagement.points.map((item) => <li><p>{item}</p></li>)}
+                </ul>
+            </section>
+            <section className='technologies-ui-section'>
+                <h4>{tech.dataManagement.title}</h4>
+                <ul className='technologies-ui-section--main-ul'>
+                    {Object.entries(tech.ui.subSections).map(([key, item]) => (
+                        <li key={key}>
+                            <h5>{item.subtitle}</h5>
+                            <ul className='technologies-ui-section--additional-ul'>
+                                {item.points.map((point, index) => (
+                                    <li key={index}>{point}</li>
+                                ))}
+                            </ul>
+                        </li>
+                    ))}
+                </ul>
+            </section>
+        </section>
     );
 };
 
